223c500c1a8547dc472149e67b6641e42a965e79
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-09-05 18:44:17 +0200

    feat(web): Install shadcn (#6)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,23 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/health': 'http://localhost:8080',
-      '/api': 'http://localhost:8080',
+      "/health": "http://localhost:8080",
+      "/api": "http://localhost:8080",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: '../crates/klens-server/static',
+    outDir: "../crates/klens-server/static",
     emptyOutDir: true,
   },
-})
+});
