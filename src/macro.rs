616d87b643fda2a3ff0eb1267c775cb62d485d9b
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-09-12 14:10:06 +0200

    chore(kafka): drop unused watermark and env helpers (#121)

--- src/macro.rs
+++ src/macro.rs
@@ -1,18 +1,6 @@
 /// Lazily parse an environment variable, falling back to a default if the
 /// variable is unset or the value cannot be parsed.
-#[allow(unused_macro_rules)]
 macro_rules! lazy_env_parse {
-    ($key:expr, Option<String>) => {
-        std::sync::LazyLock::new(|| std::env::var($key).ok())
-    };
-    ($key:expr, $t:ty) => {
-        std::sync::LazyLock::new(|| {
-            std::env::var($key)
-                .ok()
-                .and_then(|s| s.parse::<$t>().ok())
-                .unwrap_or_default()
-        })
-    };
     ($key:expr, $t:ty, || $default:expr) => {
         std::sync::LazyLock::new(|| {
             std::env::var($key)
